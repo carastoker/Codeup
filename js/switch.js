50d889a657fb8c1becf20d1dfa8a43530c9b7145
cstoker <stoker.cara@gmail.com>
2017-04-10 16:53:57 -0500

    luckyNumer project completed discount project month project

--- js/switch.js
+++ js/switch.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//
 var receipt = 60;
 var luckyNumber = Math.floor(Math.random() * 6);
 
@@ -25,3 +25,48 @@ switch (luckyNumber) {
     default:
         console.log("nada");
 }
+
+var luckyNumber = Math.floor(Math.random()* 12);
+
+switch (luckyNumber) {
+    case 1:
+        console.log ("Jan");
+        break;
+    case 2:
+        console.log ("Feb");
+        break;
+    case 3:
+        console.log ("Mar");
+        break;
+    case 4:
+        console.log ("Apr");
+        break;
+    case 5:
+        console.log ("May");
+        break;
+    case 6:
+        console.log ("Jun");
+        break;
+    case 7:
+        console.log ("Jul");
+        break;
+    case 8:
+        console.log ("Aug");
+        break;
+    case 9:
+        console.log ("Sep");
+        break;
+    case 10:
+        console.log ("Oct");
+        break;
+    case 11:
+        console.log ("Nov");
+        break;
+    case 12:
+        console.log ("Dec"  );
+        break;
+
+    default:
+        console.log("Not working");
+}
+
