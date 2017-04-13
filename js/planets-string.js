67d07f643d0cd2a558151c3585cd8f6099941287
cstoker <stoker.cara@gmail.com>
2017-04-13 14:44:02 -0500

    completed planet array exercise. completed planets-string exercise. completed planets-string bonus exercise.

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,29 +1,35 @@
 (function(){
     "use strict";
 
-    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    //
+
+    function planetsFunction() {
+        var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
 
 
-    // TODO: Convert planetsString to an array, save it to planetsArray.
+        // TODO: Convert planetsString to an array, save it to planetsArray.
 
-    var planetsArray = planetsString.split('|');
+        var planetsArray = planetsString.split('|');
 
-    console.log(planetsArray);
+        console.log(planetsArray);
 
-    // TODO: Create a string with <br> tags between each planet. console.log() your results.
+        // TODO: Create a string with <br> tags between each planet. console.log() your results.
 
-    planetsString = planetsArray.join('<br>');
+        planetsString = planetsArray.join('<br>');
 
-    document.write(planetsString);
+        document.write(planetsString);
 
-    // Why might this be useful?
+        // Why might this be useful?
 
-    // Bonus: Create a second string that would display your planets in an undordered list.
-    //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
-    //        console.log() your results.
+        // Bonus: Create a second string that would display your planets in an undordered list.
+        //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
+        //        console.log() your results.
 
-    var planetList = "<ul><li>" + planetsArray.join('</li><li>') + "</li></ul>";
-    document.write(planetList);
+        var planetList = "<ul><li>" + planetsArray.join('</li><li>') + "</li></ul>";
+        document.write(planetList);
+    }
+
+    planetsFunction();
 
 })();
 
